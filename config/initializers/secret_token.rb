241eba853d59fb87031212cee102b59a15c873e0
David Heath <david@davidheath.org>
2013-11-20 22:40:36 +0000

    Secret token should be set from env var

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-SkintDance::Application.config.secret_token = '70d4fabac63050930341c6af51a2659a0f8aa53f177cfda138dd5421f980114e614d49ddf1125481bb1bf68e70bcbe1a90b70a152ff86524e13b676d49e45bc2'
+SkintDance::Application.config.secret_token = ENV['SECRET_TOKEN']
