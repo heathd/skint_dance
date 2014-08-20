7ef43882c6c2c204314284d8fa67f75ea9b93314
David Heath <david@davidheath.org>
2014-08-20 23:29:07 +0100

    two more non-sleeping places

--- lib/reservation_manager.rb
+++ lib/reservation_manager.rb
@@ -18,8 +18,8 @@ class ReservationManager
     {
       nil => {},
       SALES_OPEN => {
-        sleeping: 80,
-        non_sleeping: 20,
+        sleeping: 78,
+        non_sleeping: 22,
         friday_evening: 80,
         saturday_daytime: 30,
         saturday_evening: 80,
